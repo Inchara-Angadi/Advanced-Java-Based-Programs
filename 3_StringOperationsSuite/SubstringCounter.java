3a77a6330d86d8affcb6cfb353e22e75e2a0b03a
Inchara-Angadi <inchara.angadi214@gmail.com>
2025-05-22 00:49:29 +0530

    Update SubstringCounter.java

--- 3_StringOperationsSuite/SubstringCounter.java
+++ 3_StringOperationsSuite/SubstringCounter.java
@@ -1,3 +1,6 @@
+/* Write a Java Program for Counting how many times a substring appears in a main string using user defined function countOccurrences()*/
+
+
 package program;
 import java.util.Scanner;
 public class SubstringCounter {
