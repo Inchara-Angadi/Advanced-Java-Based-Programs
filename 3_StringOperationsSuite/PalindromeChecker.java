3d6f5b4aa3ab30dbd24e83f17a6407eae096ea37
Inchara-Angadi <inchara.angadi214@gmail.com>
2025-05-22 00:53:31 +0530

    Update PalindromeChecker.java

--- 3_StringOperationsSuite/PalindromeChecker.java
+++ 3_StringOperationsSuite/PalindromeChecker.java
@@ -1,3 +1,6 @@
+/*Write a Java Program for Checking if a string reads the same backward as forward (ignoring case
+and punctuation) using user defined function isPalindrome():*/
+
 package program;
 import java.util.Scanner;
 public class PalindromeChecker {
