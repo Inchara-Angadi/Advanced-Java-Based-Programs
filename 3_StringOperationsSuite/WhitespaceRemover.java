63629150de4abc4b5c58e4e2781126e69c4c13e7
Inchara-Angadi <inchara.angadi214@gmail.com>
2025-05-22 00:54:58 +0530

    Update WhitespaceRemover.java

--- 3_StringOperationsSuite/WhitespaceRemover.java
+++ 3_StringOperationsSuite/WhitespaceRemover.java
@@ -1,3 +1,7 @@
+/*Write a Java Program for Eliminating all whitespace characters from a string using user defined
+function removeWhitespace()*/
+
+
 package program;
 import java.util.Scanner;
 public class WhitespaceRemover {
