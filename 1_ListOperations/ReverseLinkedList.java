0845be7ac564a1f3bb45a3c49df69e15acf300e1
Inchara-Angadi <inchara.angadi214@gmail.com>
2025-05-22 00:32:53 +0530

    Update ReverseLinkedList.java

--- 1_ListOperations/ReverseLinkedList.java
+++ 1_ListOperations/ReverseLinkedList.java
@@ -1,3 +1,6 @@
+/*Write a Java program to iterate a linked list in reverse order (using
+objlist.descendingIterator())*/
+
 package program;
 import java.util.LinkedList;
 import java.util.Iterator;
