03de41e6fc3a4a151aeebf6bc846c5a1b96cffc9
Inchara-Angadi <inchara.angadi214@gmail.com>
2025-05-22 00:55:31 +0530

    Update Capitalizewords.java

--- 3_StringOperationsSuite/Capitalizewords.java
+++ 3_StringOperationsSuite/Capitalizewords.java
@@ -1,3 +1,6 @@
+/*Write a Java Program for Capitalizing the first letter of each word. using user defined function
+capitalizeWords()*/
+
 package program;
 import java.util.Scanner;
 public class Capitalizewords {
