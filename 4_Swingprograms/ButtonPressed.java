b2508cea1d520dd4a5033bc35ff39d7c2bb533f6
Inchara-Angadi <inchara.angadi214@gmail.com>
2025-05-22 01:00:52 +0530

    Update ButtonPressed.java

--- 4_Swingprograms/ButtonPressed.java
+++ 4_Swingprograms/ButtonPressed.java
@@ -1,3 +1,7 @@
+/*Develop a Swing program in Java to display a message “Srilanka is pressed” or “India is
+pressed” depending upon the Jbutton either Srilanka or India is pressed by implementing the
+event handling mechanism with addActionListener( ).*/
+
 package program;
 import javax.swing.*;
 import java.awt.*;
